c728663967443c1c386c707119ed8d4a867fc1dc
JulienFAUJA <julienfaujanet@gmail.com>
2024-05-24 13:37:17 +0200

    [FRONT][LIAISON][GUARDS/UNGUARDS] Liaison back et front Implémentations des guards et unguards ok...

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -9,19 +9,37 @@ import { ThemesListComponent } from './pages/themes-list/themes-list.component';
 import { UserComponent } from './pages/user/user.component';
 import { ArticleDetailComponent } from './pages/article-detail/article-detail.component';
 import { ArticleCreateComponent } from './pages/article-create/article-create.component';
+import { AuthGuard } from './core/guards/auth.guards';
+import { UnauthGuard } from './core/guards/unauth.guards';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  
-  { path: 'article/detail/:id', component: ArticleDetailComponent },
-  { path: 'article/create', component: ArticleCreateComponent },
-  { path: 'article/all', component: ArticlesListComponent },
-  { path: 'themes', component: ThemesListComponent },
-  { path: 'profile', component: UserComponent },
+  { path: '', component: HomeComponent, canActivate: [UnauthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [UnauthGuard] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [UnauthGuard],
+  },
+
+  {
+    path: 'article/detail/:id',
+    component: ArticleDetailComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'article/create',
+    component: ArticleCreateComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'article/all',
+    component: ArticlesListComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'themes', component: ThemesListComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: UserComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
